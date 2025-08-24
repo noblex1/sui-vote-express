ca8e283bf5392fb64ca5276d19093e27941f72ea
noblex1 <mrgm156@gmail.com>
2025-08-24 19:54:46 +0000

    Update the struct

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { SuiClientProvider, SuiClientProviderProps } from "@mysten/dapp-kit";
+
+const networks: SuiClientProviderProps<any>["networks"] = {
+	testnet: {
+		url: "https://fullnode.testnet.sui.io:443",
+	},
+};
+
+createRoot(document.getElementById("root")!).render(
+	<SuiClientProvider networks={networks} network="testnet">
+		<App />
+	</SuiClientProvider>
+);
